a1b11963f1d6a7c1c753e293d98e58823219c423
ProgrammerLP <developer.programmerlp@gmx.net>
2023-09-30 23:28:57 +0200

    Projektdateien hinzufügen.

---
+++ InfoHub/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InfoHub
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
